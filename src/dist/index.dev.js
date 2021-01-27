462b42348d03de8bd9efd595a714d85f68aa858e
dtrujillog <62666785+dtrujillog@users.noreply.github.com>
2021-01-26 21:24:56 -0600

    20212601
    Cambio de puerto Por Dieguito en Megamaster

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var express = require('express');
-const { Server } = require('http');
 
 var app = express();
 
@@ -11,13 +10,9 @@ var path = require('path'); // app.use(exress.urlencoded({extended: false}));
 app.use(express.urlencoded({
   extended: false
 }));
-app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(require('./routes/index'));
 app.use(express["static"](path.join(__dirname, 'public')));
-// app.listen(3000, function () {
-//   console.log('Servidor en el puerto 3000 ahuevo');
-// }
-server.listenerCount(app.get('port'),()=>{
-console.log('Server on port ${app.get("port")}');
+app.listen(80, function () {
+  console.log('Servidor en el puerto 80 ahuevo');
 });
